6d61954e60405158c53388502360891cef159ca8
sacz18 <123790220+sacz18@users.noreply.github.com>
2026-01-30 20:25:37 -0600

    Personalize greeting message with query parameter

--- o	api/saludo.js
+++ o	api/saludo.js
@@ -1,7 +1,10 @@
 export default function handler(req, res) {
+  const nombre = req.query.nombre || "anónimo";
+
   res.status(200).json({
-    mensaje: "Hola ISW2",
+    mensaje: `Hola ${nombre}`,
     curso: "Ingeniería de Software II",
     timestamp: new Date().toISOString()
   });
 }
+
