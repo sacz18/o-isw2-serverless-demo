2994d725f823ca45ad98c338ae94373f99faeb71
sacz18 <123790220+sacz18@users.noreply.github.com>
2026-02-03 15:04:22 -0600

    Add error simulation and timestamp to response
    Simulate an error response for 'error' input and add a timestamp to the response.

--- o	api/procesar.js
+++ o	api/procesar.js
@@ -1,7 +1,20 @@
 export default function handler(req, res) {
   const nombre = req.query.nombre || "anónimo";
 
+  //SIMULAR ERROR si nombre = "error" (MINI-RETO)
+  if (nombre.toLowerCase() === "error") {
+    return res.status(500).json({
+      error: "Error simulado intencionalmente"
+    });
+  }
+
+   //AGREGAR TIMESTAMP (MINI-RETO)
+  const timestamp = new Date().toISOString();
+  
+  //normalmente pasa esto
   res.status(200).json({
-    resultado: `Nombre procesado: ${nombre.toUpperCase()}`
+    resultado: `Nombre procesado: ${nombre.toUpperCase()}`,
+    timestamp: timestamp
   });
+    
 }
